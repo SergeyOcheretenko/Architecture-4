2d53c6eba83a16d236c2eb24f5277de98ed2b85a
juliion <juliaionova111@gmail.com>
2022-06-22 17:12:10 +0300

    Add errors handler.

--- main.go
+++ main.go
@@ -13,15 +13,30 @@ import (
 func parse(cmdLine string) eventloop.Command {
 	var cmd eventloop.Command
 	partsOfCmd := strings.Fields(cmdLine)
+	length := len(partsOfCmd)
+	if length == 0 {
+		cmd = commands.PrintCmd{Text: "Error: there is no command"}
+		return cmd
+	}
 	cmdName := partsOfCmd[0]
 	if strings.Compare(cmdName, "print") == 0 {
+		if length == 1 {
+			cmd = commands.PrintCmd{Text: "Error: lacks the arguments for print"}
+			return cmd
+		}
 		arg := partsOfCmd[1]
 		cmd = commands.PrintCmd{Text: arg}
 	} else if strings.Compare(cmdName, "printc") == 0 {
+		if length <= 2 {
+			cmd = commands.PrintCmd{Text: "Error: lacks the arguments for printс"}
+			return cmd
+		}
 		firstArgStr := partsOfCmd[1]
 		firstArgInt, _ := strconv.Atoi(firstArgStr)
 		secondArg := partsOfCmd[2]
 		cmd = commands.PrintcCmd{Count: firstArgInt, Symbol: secondArg}
+	} else {
+		cmd = commands.PrintCmd{Text: "Error: unknown command"}
 	}
 	return cmd
 }
