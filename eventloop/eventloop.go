265d5e924c12850a43acb08bebaa19223f017f90
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2022-06-22 16:11:48 +0300

    Add eventloop Post command

--- eventloop/eventloop.go
+++ eventloop/eventloop.go
@@ -13,12 +13,27 @@ func (eLoop *EventLoop) Start() {
 		cond: *sync.NewCond(&sync.Mutex{}),
 	}
 	eLoop.isStopped = make(chan struct{})
-	// listenCommands ?
+	go eLoop.listenCommands()
+}
+
+func (eLoop *EventLoop) listenCommands() {
+	for !eLoop.stop || !eLoop.commands.isEmpty() {
+		cmd := eLoop.commands.pull()
+		cmd.Execute(eLoop)
+	}
+	eLoop.isStopped <- struct{}{}
+}
+
+func (eLoop *EventLoop) Post(cmd Command) {
+	if eLoop.stop {
+		return
+	}
+	eLoop.commands.push(cmd)
 }
 
 func (eLoop *EventLoop) AwaitFinish() {
-	// eLoop.Post(CommandFunc(func(handler Handler) {
-	//
-	// }))
+	eLoop.Post(CommandFunc(func(handler Handler) {
+		eLoop.stop = true
+	}))
 	<-eLoop.isStopped
 }
